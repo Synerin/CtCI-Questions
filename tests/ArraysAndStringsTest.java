f92c27cc66fb184dcf588401ab424a6296374297
Synerin <36575784+Synerin@users.noreply.github.com>
2020-08-12 17:37:50 -0400

    create URLify tests before writing out method for TDD

--- tests/ArraysAndStringsTest.java
+++ tests/ArraysAndStringsTest.java
@@ -17,10 +17,31 @@ public class ArraysAndStringsTest {
     }
 
     @Test
-    public void checkPermutation() {
+    public void checkPermutationTest() {
         assertTrue(test.checkPermutation("abc", "bca"));
         assertTrue(test.checkPermutation("abc", "abc"));
         assertFalse(test.checkPermutation("abc ", "cba"));
         assertTrue(test.checkPermutation("abc  ", " abc "));
+    }
+
+    @Test
+    public void URLifyTest() {
+        char[] test1, test2;
+
+        test1 = "Mr John Smith   ".toCharArray();
+        test2 = "Mr%20John%20Smith".toCharArray();
+        assertEquals(test.URLify(test1, 13), test2);
+
+        test1 = " ".toCharArray();
+        test2 = new char[] {'\0'};
+        assertEquals(test.URLify(test1, 0), test2);
+
+        test1 = "Hello".toCharArray();
+        test2 = test1;
+        assertEquals(test.URLify(test1, 5), test2);
+
+        test1 = "a   b".toCharArray();
+        test2 = "a%20%20%20b".toCharArray();
+        assertEquals(test.URLify(test1, 11), test2);
     }
 }
