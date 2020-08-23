3ec4e68ca7a1a2cf685cdc6338290c55ef90ecf4
Synerin <36575784+Synerin@users.noreply.github.com>
2020-08-22 23:31:54 -0400

    create tests for rotateMatrix method

--- tests/ArraysAndStringsTest.java
+++ tests/ArraysAndStringsTest.java
@@ -78,4 +78,20 @@ public class ArraysAndStringsTest {
         assertEquals(test.stringCompression("abcbabcbabb"), "abcbabcbabb");
         assertEquals(test.stringCompression(""), "");
     }
+
+    @Test
+    public void rotateMatrixTest() {
+        int[][] actual, expected;
+
+        actual = new int[][] {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16}};
+        expected = new int[][] {{13,9,5,1}, {14,10,6,2}, {15,11,7,3}, {16,12,8,4}};
+        assertArrayEquals(test.rotateMatrix(actual), expected);
+
+        actual = new int[][] {{1}};
+        expected = new int[][] {{1}};
+        assertArrayEquals(test.rotateMatrix(actual), expected);
+
+        actual = new int[][] {{1,0}, {1,0}};
+        expected = new int[][] {{1, 1}, {0, 0}};
+    }
 }
